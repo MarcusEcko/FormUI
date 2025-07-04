0edd20c815d4ab1000be98a83f47c3a331a8018f
MarcusEcko <marco237.mnm@gmail.com>
2025-07-04 01:16:34 -0300

    submit btn added to js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ class FormularioNombre extends React.Component {
       nombre: "",
       email: "",
       passwd: "",
-      confirmpasswd: ""
+      confirmpasswd: "",
+      submit : ""
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -19,36 +21,49 @@ class FormularioNombre extends React.Component {
     this.setState( { [name]: value } );
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState( {submit: this.state.submit });
+  }
+
+  /*    check password   */
   passwdMatch() {
     return this.state.passwd === this.state.confirmpasswd;
   }
 
   render () {
     return(
-      <div>
-        <label>Nombre completo:</label>
-        <input type="text" name='nombre' value={this.state.nombre} onChange={this.handleChange} />
-        <p>Tu nombre es: { this.state.nombre ? this.state.nombre : "Aún no se ha insertado el nombre" } </p>
-        
-        <label>E-Mail:</label>
-        <input type="email" name='email' value={this.state.email} onChange={this.handleChange}/>
-        <p>Tu email es: {this.state.email ? this.state.email : "email no insertado" }</p>
-      
-        {/* PASSWORD */}
-        <label>Inserta la Contraseña:</label>
-        <input type='password' name="passwd" value={this.state.passwd} onChange={this.handleChange} />
-        <p>Tu contraseña es: 
-          {this.state.passwd ?  "•".repeat(this.state.passwd.length) : "no ingresada"}
-        </p>
 
-        <label>Confirma la Contraseña:</label>
-        <input type="password" name="confirmpasswd" value={this.setState.confirmpasswd} onChange={this.handleChange} />
-        <p>
-          {/* confirm passwrd */}
-          Estado: { this.state.confirmpasswd && 
-            this.passwdMatch() ? "Las contraseñas coinciden ✅" : "Las contraseñas no coinciden, intentalo de nuevo.❌"
-          }
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Nombre completo:</label>
+          <input type="text" name='nombre' value={this.state.nombre} onChange={this.handleChange} />
+          <p>Tu nombre es: { this.state.nombre ? this.state.nombre : "Aún no se ha insertado el nombre" } </p>
+          
+          <label>E-Mail:</label>
+          <input type="email" name='email' value={this.state.email} onChange={this.handleChange}/>
+          <p>Tu email es: {this.state.email ? this.state.email : "email no insertado" }</p>
+        
+          {/* PASSWORD */}
+          <label>Inserta la Contraseña:</label>
+          <input type='password' name="passwd" value={this.state.passwd} onChange={this.handleChange} />
+          <p>Tu contraseña es: 
+            {this.state.passwd ?  "•".repeat(this.state.passwd.length) : "no ingresada"}
+          </p>
+
+          <label>Confirma la Contraseña:</label>
+          <input type="password" name="confirmpasswd" value={this.setState.confirmpasswd} onChange={this.handleChange} />
+          <p>
+            {/* check passwrd */}
+            Estado: { this.state.confirmpasswd && 
+              this.passwdMatch() ? "Las contraseñas coinciden ✅" : "Las contraseñas no coinciden, intentalo de nuevo.❌"
+            }
+          </p>
+
+          <button type="submit">Update Profile</button>
+          <h1>{this.state.submit}</h1>
+        </form>
+
       </div>
     )
   }
